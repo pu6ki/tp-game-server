3d13b791102c105ef75318fbbb70e7f9de5f630f
wencakisa <wencakisa@gmail.com>
2018-01-23 16:57:08 +0200

    Added basic levels structure

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,62 @@
+class LevelsController < ApplicationController
+  before_action :set_level, only: [:show, :edit, :update, :destroy]
+
+  # GET /levels
+  def index
+    @levels = Level.all
+  end
+
+  # GET /levels/1
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.js   { render plain: @level.code }
+    end
+  end
+
+  # GET /levels/new
+  def new
+    @level = Level.new
+  end
+
+  # GET /levels/1/edit
+  def edit
+  end
+
+  # POST /levels
+  def create
+    @level = Level.new(level_params)
+
+    if @level.save
+      redirect_to @level, notice: 'Level was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /levels/
+  def update
+    if @level.update(level_params)
+      redirect_to @level, notice: 'Level was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /levels/1
+  def destroy
+    @level.destroy
+    redirect_to levels_url, notice: 'Level was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_level
+    @level = Level.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def level_params
+    params.require(:level).permit(:name, :code, :user_id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123135805) do
+ActiveRecord::Schema.define(version: 20180123143755) do
+
+  create_table "levels", force: :cascade do |t|
+    t.string "name"
+    t.text "code", limit: 16777216
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_levels_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
