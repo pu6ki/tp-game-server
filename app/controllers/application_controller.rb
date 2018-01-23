fc31ea027e69875559e335b9e9b3d83da43ff262
wencakisa <wencakisa@gmail.com>
2018-01-23 22:09:45 +0200

    Removed forgery protection in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception, unless: proc { js_request? || json_request? }
-  protect_from_forgery with: :null_sessions
-  private
-
-  def js_request?
-    request.format.js?
-  end
-
-  def json_request?
-    request.format.json?
-  end
+  protect_from_forgery with: :null_session
 end
