2aa51d54b43a0934cda830bb4a684ece320b50d2
wencakisa <wencakisa@gmail.com>
2018-01-23 22:27:47 +0200

    Fixed bug in levels find query

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -57,7 +57,7 @@ class LevelsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_level
-    @level = current_user.levels.find(params[:id])
+    @level = Level.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
